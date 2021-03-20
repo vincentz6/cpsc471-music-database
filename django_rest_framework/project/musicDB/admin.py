e8e575e14ba99c0f03fcc9d003eb7b1d8a62265b
MehdiMarzban-eng <53926498+MehdiMarzban-eng@users.noreply.github.com>
2021-03-20 17:41:50 -0600

    Added Album, Song, Label, Playlist, Genre

--- django_rest_framework/project/musicDB/admin.py
+++ django_rest_framework/project/musicDB/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from . models import artist
+from . models import Artist
+#'''
+from . models import Song
+from . models import Genre
+from . models import Album
+from . models import Label
+from . models import Playlist
+#'''
 # Register your models here.
 
-admin.site.register(artist)
+admin.site.register(Artist)
+#'''
+admin.site.register(Song)
+admin.site.register(Genre)
+admin.site.register(Album)
+admin.site.register(Label)
+admin.site.register(Playlist)
+
+#'''
